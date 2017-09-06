95126ded674212fb07bdc98072208c59480b34ea
Marlon Gumban <msg2msg2@gmail.com>
2017-09-06 22:25:40 +0800

    .

--- src/user.js
+++ src/user.js
@@ -5,6 +5,6 @@ const UserSchema = new Schema ({
     name: String
 });
 
-const User = mongoose.model("user", UserSchema);
+const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
