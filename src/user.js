4f5c9a9fffbd09a1b68cc521c00e5cdcbe909ee9
Marlon Gumban <msg2msg2@gmail.com>
2017-09-09 18:44:39 +0800

    update

--- src/user.js
+++ src/user.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const PostSchema = require("./post");
+
 const UserSchema = new Schema ({
     name: {
         type: String,
+        validate: {
+            validator: (name) => name.length>2,
+            message: 'Name must be longer than 2 characters.'
+        },
         required: [true, 'Name is required.']
     },
-    postCount: Number
+    postCount: Number,
+    posts: [PostSchema]
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/validatation_test.js
+++ test/validatation_test.js
@@ -12,6 +12,26 @@ describe('Validating Records', () => {
 
         });
         */
+        const {message} = validationResult.errors.name;
+        assert(message === 'Name is required.')
+    });
 
+    it('requires a user name longer than2  characters', () => {
+        const user = new User({ name: 'Al' });
+        const validationResult = user.validateSync();
+        const {message} = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+    });
+
+    it('disallows invalid records from being saved', (done) => {
+        const user = new User ({ name: 'Al' });
+        user.save()
+            .catch((validationResult) => {
+                const {message} = validationResult.errors.name;
+
+                assert(message === 'Name must be longer than 2 characters.');
+                done();
+            });
     });
 });
