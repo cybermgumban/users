c581200cc1f5211880a89912ce042e743b6fb310
Marlon Gumban <msg2msg2@gmail.com>
2017-09-09 11:19:06 +0800

    .

--- test/create_test.js
+++ test/create_test.js
@@ -3,9 +3,18 @@ const User = require("../src/user");
 
 describe('Creating records', () => {
     it('saves a user', () => {
+        //this will test if your it statement is working before you put in any command
+        //assert(1+1 === 3);
+
         const joe = new User({ name: "Joe" });
 
-        joe.save();
+        joe.save()
+            .then(() => {
+                //Has joe been saved successfully?
+                //joe.isNew === true if isn't saved yet in Mongo and still in Mocha
+                assert(!joe.isNew);
+                done();
+            });
     });
 
 });
